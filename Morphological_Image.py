5eb0a4afda6b89484b75088373ab7bd4c94b37c3
O.M.Gibran <omargibran745@gmail.com>
2024-11-04 23:10:04 +0700

    change hoodie segmentation color

--- Morphological_Image.py
+++ Morphological_Image.py
@@ -19,19 +19,12 @@ else:
 hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
 # Define color ranges for segmentation based on the colors in the image
-# Adjust these values if needed to fit the specific shades in your image
-
-# Jacket (Gray with Green Hood)
 lower_gray = np.array([0, 0, 40])
 upper_gray = np.array([180, 30, 200])
 lower_green = np.array([40, 40, 40])
 upper_green = np.array([80, 255, 255])
-
-# Pants (Black)
 lower_black = np.array([0, 0, 0])
 upper_black = np.array([180, 255, 50])
-
-# Skin (we'll use a generic skin tone range here, may need adjustment)
 lower_skin = np.array([0, 20, 70])
 upper_skin = np.array([20, 255, 255])
 
@@ -49,6 +42,7 @@ masked_output = np.zeros_like(image)
 masked_output[mask_jacket > 0] = [128, 128, 128]  # Gray for jacket
 masked_output[mask_black > 0] = [0, 0, 0]         # Black for pants
 masked_output[mask_skin > 0] = [0, 255, 0]        # Green for skin
+masked_output[mask_green > 0] = [0, 0, 255]      # Change green hoodie to red
 
 # Apply transparency mask to keep only the main character area
 masked_output[~mask] = [0, 0, 0]  # Set background to black
